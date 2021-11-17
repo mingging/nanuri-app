f523c34785a1c1726975a7050ea4d3ad91440a4c
mingging <minimani0421@gmail.com>
2021-11-17 21:06:06 +0900

    create notice view

---
+++ nanuri-app/nanuri-app/Utils/Names.swift
@@ -0,0 +1,22 @@
+//
+//  Names.swift
+//  nanuri-app
+//
+//  Created by minimani on 2021/11/17.
+//
+
+import Foundation
+
+enum XibName {
+    static let productCustomCell = "ProductCustomCell"
+    static let noticeCustomCell = "NoticeCustomCell"
+}
+
+enum ImageName {
+    static let searchIcon = "search_icon"
+    static let noticeIcon = "notice_icon"
+}
+
+enum StoryboardID {
+    static let notice = "notice"
+}
