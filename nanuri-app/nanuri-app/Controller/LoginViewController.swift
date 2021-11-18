948572e85ed377052adf37bda1b70ae2634738dc
Seohyun_Kim <cestbonciel@gmail.com>
2021-11-18 17:00:30 +0900

    create login view

---
+++ nanuri-app/nanuri-app/Controller/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  nanuri-app
+//
+//  Created by ShKim on 2021/11/18.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var kakaoLogin: UIButton!
+    @IBOutlet weak var appleLogin: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
