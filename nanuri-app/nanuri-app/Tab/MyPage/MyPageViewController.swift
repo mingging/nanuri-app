3d04cf4402ff0ebdcbcf06f2ce99aa24ac97efb6
Seohyun Kim <cestbonciel88@knou.ac.kr>
2021-11-25 22:04:06 +0900

    add mypage view - first commit

---
+++ nanuri-app/nanuri-app/Tab/MyPage/MyPageViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MyPageViewController.swift
+//  nanuri-app
+//
+//  Created by a0000 on 2021/11/19.
+//
+
+import UIKit
+
+class MyPageViewController: HeaderViewController {
+    
+    let items = ["내가 구매한 상품", "내가 등록한 상품"]
+   
+
+    //constraint를 설정해주기 위해 false를 해야한다.
+   
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func addTarget(){
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
