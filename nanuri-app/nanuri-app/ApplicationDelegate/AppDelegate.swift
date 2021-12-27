c05e75a3fa171d1ab190fff09a36e0a01821e2d4
min <75626237+mingging@users.noreply.github.com>
2021-12-28 00:46:46 +0900

    edit product Cell with Snapkit

--- nanuri-app/nanuri-app/ApplicationDelegate/AppDelegate.swift
+++ nanuri-app/nanuri-app/ApplicationDelegate/AppDelegate.swift
@@ -9,10 +9,9 @@ import UIKit
 import DropDown
 import KakaoSDKAuth
 import KakaoSDKCommon
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
